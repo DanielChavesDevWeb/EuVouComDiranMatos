98dcb6a402042801d84630efc5c4535b19726ac0
Daniel Chaves <danielpchaves080@gmail.com>
2024-04-30 02:42:33 -0300

    update-imgs-functions

--- script.js
+++ script.js
@@ -12,3 +12,19 @@ function tradeBtn(){
         btn.classList.remove('fa-x')
     }
 }
+// Função para verificar a largura da tela e trocar o src da imagem conforme necessário
+function alterarSrcImagem() {
+    const larguraDaTela = window.innerWidth; // Obtém a largura da tela
+    const img = document.getElementById('profile-img'); // Obtém a referência para a imagem
+
+    // Verifica se a largura da tela é menor que 1400px
+    if (larguraDaTela < 1400) {
+        img.src = 'assets/profile-2.png'; // Substitui o src da imagem pela nova imagem
+    } else {
+        img.src = 'assets/profile.png'; // Retorna ao src original se a largura da tela for maior ou igual a 1400px
+    }
+}
+
+// Chama a função quando a página é carregada e quando a janela é redimensionada
+window.onload = alterarSrcImagem;
+window.onresize = alterarSrcImagem;
