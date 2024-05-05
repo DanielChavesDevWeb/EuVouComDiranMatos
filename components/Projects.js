7526bcfdb5eeba897113eca13d248540e8782c2e
Daniel Chaves <danielpchaves080@gmail.com>
2024-05-05 08:04:22 -0300

    updateModalProjects

---
+++ components/Projects.js
@@ -0,0 +1,25 @@
+let Projects = [
+    {
+        titulo: "Titulo projeto 1",
+        img: "./assets/projeto1.png",
+        text1: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius aliquam quae repudiandae temporibus rem atque a eum vitae, cum quaerat ratione. Necessitatibus alias, dolorum facilis totam accusamus quod vero magni? Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem qui quas obcaecati reprehenderit, neque nisi doloribus debitis nostrum iste numquam esse sed reiciendis expedita, beatae ab eos officiis! Nulla, commodi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, optio perspiciatis! Eaque, recusandae at quisquam ipsam, voluptatem quasi doloremque praesentium aut facere molestias molestiae neque commodi distinctio, impedit sequi corrupti?",
+        text2: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus nihil quam et doloremque voluptatibus dolorem animi, dolore officia repudiandae velit sunt! Quam, iure libero nihil numquam officiis laborum aliquam tempore! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat, praesentium! Sapiente atque placeat autem ab cumque, voluptate alias possimus quidem quo? Ad, eos delectus! Voluptatum maiores laborum necessitatibus sint ea? Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae sunt ea animi nihil et officiis consequatur facilis saepe, recusandae quisquam. Quam dicta incidunt reiciendis explicabo animi assumenda doloremque labore voluptas!",
+        video: "https://www.youtub.com/embed/WZG1lVqNHec"
+    },
+    {
+        titulo: "Titulo projeto 2",
+        img: "./assets/projeto2.jpg",
+        text1: "texto1 projeto 2 ipsum dolor sit amet consectetur adipisicing elit. Eius aliquam quae repudiandae temporibus rem atque a eum vitae, cum quaerat ratione. Necessitatibus alias, dolorum facilis totam accusamus quod vero magni? Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem qui quas obcaecati reprehenderit, neque nisi doloribus debitis nostrum iste numquam esse sed reiciendis expedita, beatae ab eos officiis! Nulla, commodi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, optio perspiciatis! Eaque, recusandae at quisquam ipsam, voluptatem quasi doloremque praesentium aut facere molestias molestiae neque commodi distinctio, impedit sequi corrupti?",
+        text2: "texto2 projeto 2 ipsum dolor sit amet, consectetur adipisicing elit. Repellendus nihil quam et doloremque voluptatibus dolorem animi, dolore officia repudiandae velit sunt! Quam, iure libero nihil numquam officiis laborum aliquam tempore! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat, praesentium! Sapiente atque placeat autem ab cumque, voluptate alias possimus quidem quo? Ad, eos delectus! Voluptatum maiores laborum necessitatibus sint ea? Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae sunt ea animi nihil et officiis consequatur facilis saepe, recusandae quisquam. Quam dicta incidunt reiciendis explicabo animi assumenda doloremque labore voluptas!",
+        video: "https://www.youtub.com/embed/-SN5bLLS4hI?si=TCkQYCCDDaXnyHIk"
+    },
+    {
+        titulo: "Titulo projeto 3",
+        img: "./assets/projeto3.jpg",
+        text1: "texto1 projeto 3 Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius aliquam quae repudiandae temporibus rem atque a eum vitae, cum quaerat ratione. Necessitatibus alias, dolorum facilis totam accusamus quod vero magni? Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem qui quas obcaecati reprehenderit, neque nisi doloribus debitis nostrum iste numquam esse sed reiciendis expedita, beatae ab eos officiis! Nulla, commodi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, optio perspiciatis! Eaque, recusandae at quisquam ipsam, voluptatem quasi doloremque praesentium aut facere molestias molestiae neque commodi distinctio, impedit sequi corrupti?",
+        text2: "texto2 projeto 3 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus nihil quam et doloremque voluptatibus dolorem animi, dolore officia repudiandae velit sunt! Quam, iure libero nihil numquam officiis laborum aliquam tempore! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat, praesentium! Sapiente atque placeat autem ab cumque, voluptate alias possimus quidem quo? Ad, eos delectus! Voluptatum maiores laborum necessitatibus sint ea? Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae sunt ea animi nihil et officiis consequatur facilis saepe, recusandae quisquam. Quam dicta incidunt reiciendis explicabo animi assumenda doloremque labore voluptas!",
+        video: "https://www.youtub.com/embed/5XEKoy3ULLg?si=3SVL4YXX3BTGikVA"
+    }
+]
+
+export default Projects
--- script.js
+++ script.js
@@ -1,35 +1,115 @@
+
 // trocar a cor e icone do button toggle menu
 
 const btnToggleMenu = document.querySelector(".btn-toggle").addEventListener("click", tradeBtn)
 
-function tradeBtn(){
+function tradeBtn() {
     let btn = this
-    if(btn.classList.contains('fa-bars')){
+    if (btn.classList.contains('fa-bars')) {
         btn.classList.remove('fa-bars')
         btn.classList.add('fa-x')
-    }else if(btn.classList.contains('fa-x')){
+    } else if (btn.classList.contains('fa-x')) {
         btn.classList.add('fa-bars')
         btn.classList.remove('fa-x')
     }
 }
+//animações do site intersectionObserver eixoX
 
+const allSection = document.querySelectorAll(".hiddenX")
+
+const observeSection = new IntersectionObserver((sections) => {
+    sections.forEach((section) => {
+        if (section.isIntersecting) {
+            section.target.classList.add("showX")
+        }
+    })
+})
+
+allSection.forEach((element) => observeSection.observe(element))
+
+//animações do site intersectionObserver eixoY
+
+const allItens = document.querySelectorAll(".hiddenY")
+
+const observeItens = new IntersectionObserver((allItens) => {
+    allItens.forEach((item) => {
+        if (item.isIntersecting) {
+            item.target.classList.add("showY")
+        }
+    })
+})
+
+allItens.forEach((item) => observeItens.observe(item))
 // configurando mensagem personalizada
 
 let btnMsg = document.querySelector("#btn-mensage").addEventListener("click", sendMsg)
 
-function sendMsg(){
+function sendMsg() {
     let assunto = document.querySelector("#assunto").value;
     let msg = document.querySelector("#mensagem").value;
 
-    if(assunto.length === 0 || msg.length === 0){
+    if (assunto.length === 0 || msg.length === 0) {
         alert("ERRO! Preencha o formulário.");
     } else {
         // Construir o link do WhatsApp com o número de telefone, assunto e mensagem
         let numeroTelefone = "557399354445"; // Substitua pelo número de telefone desejado
-        let mensagemEncoded = encodeURIComponent(`*Assunto:* ${assunto}\n\n*Mensagem:* ${msg}` , '_blank'); // Codifica a mensagem para ser incluída na URL
+        let mensagemEncoded = encodeURIComponent(`*Assunto:* ${assunto}\n\n*Mensagem:* ${msg}`, '_blank'); // Codifica a mensagem para ser incluída na URL
         let linkWhatsApp = `https://api.whatsapp.com/send?phone=${numeroTelefone}&text=${mensagemEncoded}`;
 
         // Redirecionar o usuário para o WhatsApp com a mensagem pré-preenchida
         window.open(linkWhatsApp);
     }
 }
+
+// configurando modal de projetos e botões
+
+import Projects from "./components/Projects.js"
+
+const closeProject = document.querySelector("#closeProject").addEventListener("click", exitProject)
+const aboutProject = document.querySelector("#aboutProject")
+const moreProjects = document.querySelectorAll(".btn-project")
+const btnOptionProject = document.querySelector("#opinarProjeto").addEventListener("click", optionProject)
+
+moreProjects.forEach((element, index) => {
+    element.addEventListener("click", () => {
+        showProject(index)
+    })
+})
+
+function showProject(index) {
+    aboutProject.classList.remove("hiddenProjects")
+    aboutProject.classList.add("showProjects")
+
+    validateAndShow(Projects[index])
+}
+
+function validateAndShow(project) {
+    let title = document.querySelector("#aboutTitleProject")
+    let img = document.querySelector("#imgAboutProject")
+    let text1 = document.querySelector("#text1AboutProject")
+    let text2 = document.querySelector("#text2AboutProject")
+    let video = document.querySelector("#videoAboutProject")
+    title.innerHTML = `${project.titulo}`
+    img.src = `${project.img}`
+    text1.innerHTML = `${project.text1}`
+    text2.innerHTML = `${project.text2}`
+    video.src = `${project.video}`
+}
+
+
+
+function optionProject() {
+    let assunt = document.querySelector("#aboutTitleProject").textContent
+    let phoneNumber = "557399354445";
+    let msg = encodeURIComponent(`*Olá pré-candidato gostaria de opinar no projeto:* ${assunt}`, '_blank'); // Codifica a mensagem para ser incluída na URL
+    let openApp = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${msg}`;
+
+    // Redirecionar o usuário para o WhatsApp com a mensagem pré-preenchida
+    window.open(openApp);
+}
+
+function exitProject() {
+    aboutProject.classList.add("hiddenProjects")
+    aboutProject.classList.remove("showProjects")
+}
+
