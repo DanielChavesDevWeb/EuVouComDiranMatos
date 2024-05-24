63cb2a660445a19a9fa81c89078ad187822f85cf
Daniel Chaves <danielpchaves080@gmail.com>
2024-05-24 18:27:23 -0300

    update-content

--- components/Projects.js
+++ components/Projects.js
@@ -1,14 +1,24 @@
 let Projects = [
     {
-        titulo: "Titulo projeto 1",
-        img: "./assets/projeto1.png",
-        text1: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius aliquam quae repudiandae temporibus rem atque a eum vitae, cum quaerat ratione. Necessitatibus alias, dolorum facilis totam accusamus quod vero magni? Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem qui quas obcaecati reprehenderit, neque nisi doloribus debitis nostrum iste numquam esse sed reiciendis expedita, beatae ab eos officiis! Nulla, commodi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, optio perspiciatis! Eaque, recusandae at quisquam ipsam, voluptatem quasi doloremque praesentium aut facere molestias molestiae neque commodi distinctio, impedit sequi corrupti?",
-        text2: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus nihil quam et doloremque voluptatibus dolorem animi, dolore officia repudiandae velit sunt! Quam, iure libero nihil numquam officiis laborum aliquam tempore! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat, praesentium! Sapiente atque placeat autem ab cumque, voluptate alias possimus quidem quo? Ad, eos delectus! Voluptatum maiores laborum necessitatibus sint ea? Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae sunt ea animi nihil et officiis consequatur facilis saepe, recusandae quisquam. Quam dicta incidunt reiciendis explicabo animi assumenda doloremque labore voluptas!",
+        titulo: "Projeto matrícula digital",
+        img: "./assets/matricula-digital-img.png",
+        text1: "<h4>Modernizando o Processo de Matrículas Escolares:</h4><br> Rumo a uma Inscrição Digital Como sabemos, o processo de matrícula escolar pode ser um momento desafiador, muitas vezes marcado por longas filas, papelada extensa e uma dose considerável de estresse.No entanto, estamos animados em propor uma solução que pode transformar completamente essa experiência: a transição para um sistema de matrícula digital. Ao adotar um sistema de matrícula digital, podemos eliminar as filas e tornar todo o processo mais conveniente e eficiente para todos os envolvidos.",
+        text2: "Aqui estão alguns dos benefícios que a mudança para a matrícula digital pode trazer:"
+        +"<p>"
+            +"<ol>"
+            +"<li>Conveniência: Pais e responsáveis poderão completar o processo de matrícula a qualquer momento e de qualquer lugar, sem a necessidade de visitar a escola pessoalmente.</li>"
+            +"<li>Redução de Filas e Tempo de Espera: Adeus longas filas! Com a matrícula digital, não haverá mais a necessidade de esperar por horas para inscrever seu filho na escola.</li>"
+            +"<li>Acesso Simplificado a Informações: Todas as informações necessárias para a matrícula estarão disponíveis online, facilitando o acesso e evitando confusões.</li>"
+            +"<li>Maior Eficiência Administrativa: O processo digital pode ajudar a reduzir a carga de trabalho administrativo, permitindo que nossa equipe se concentre em outras tarefas importantes.</li>"
+            +"<li>Sustentabilidade: A transição para a matrícula digital é também um passo em direção à sustentabilidade, reduzindo a necessidade de papel e outros recursos.Portanto, gostaríamos de ouvir suas opiniões e feedback sobre essa proposta.</li>"
+            + "</ol>"
+        +"</p>"
+        +"<p>Estamos animados com o potencial que a matrícula digital tem para melhorar, juntos, podemos tornar esse processo mais simples, eficiente e acessível. Obrigado, Diran Matos.</p>",
         video: "https://www.youtub.com/embed/WZG1lVqNHec"
     },
     {
         titulo: "Titulo projeto 2",
-        img: "./assets/projeto2.jpg",
+        img: "./assets/saude-digital.png",
         text1: "texto1 projeto 2 ipsum dolor sit amet consectetur adipisicing elit. Eius aliquam quae repudiandae temporibus rem atque a eum vitae, cum quaerat ratione. Necessitatibus alias, dolorum facilis totam accusamus quod vero magni? Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem qui quas obcaecati reprehenderit, neque nisi doloribus debitis nostrum iste numquam esse sed reiciendis expedita, beatae ab eos officiis! Nulla, commodi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, optio perspiciatis! Eaque, recusandae at quisquam ipsam, voluptatem quasi doloremque praesentium aut facere molestias molestiae neque commodi distinctio, impedit sequi corrupti?",
         text2: "texto2 projeto 2 ipsum dolor sit amet, consectetur adipisicing elit. Repellendus nihil quam et doloremque voluptatibus dolorem animi, dolore officia repudiandae velit sunt! Quam, iure libero nihil numquam officiis laborum aliquam tempore! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat, praesentium! Sapiente atque placeat autem ab cumque, voluptate alias possimus quidem quo? Ad, eos delectus! Voluptatum maiores laborum necessitatibus sint ea? Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae sunt ea animi nihil et officiis consequatur facilis saepe, recusandae quisquam. Quam dicta incidunt reiciendis explicabo animi assumenda doloremque labore voluptas!",
         video: "https://www.youtub.com/embed/-SN5bLLS4hI?si=TCkQYCCDDaXnyHIk"
